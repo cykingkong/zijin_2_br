5a35861d3c7d3250783c37226a2cff78ce11e3cf
cykingkong <847939497@qq.com>
2025-07-02 22:29:30 +0800

    ipo配资在status不为1时不让点击

--- src/pages/ipo/component/discont-item.vue
+++ src/pages/ipo/component/discont-item.vue
@@ -1,20 +1,12 @@
 <template>
-  <div
-    class="discont-item-content w-full px-12 py-24 flex flex-col gap-20px"
-    v-if="props.itemType == 'ipo'"
-  >
+  <div class="discont-item-content w-full px-12 py-24 flex flex-col gap-20px" v-if="props.itemType == 'ipo'">
     <ipoItemTop :item="_item"></ipoItemTop>
     <ipoItemCenter :item="_item"></ipoItemCenter>
     <ipoItemInfo :item="_item"></ipoItemInfo>
     <div class="li">
       <div class="li-l w-70%">
-        <van-progress
-          :percentage="_item.percentage"
-          stroke-width="8px"
-          track-color="#1989fa"
-          color="#e5e5e5"
-          :show-pivot="false"
-        />
+        <van-progress :percentage="_item.percentage" stroke-width="8px" track-color="#1989fa" color="#e5e5e5"
+          :show-pivot="false" />
       </div>
     </div>
     <div class="flex gap-12">
@@ -24,42 +16,24 @@
         }}</van-button>
       </div>
       <div class="flex-1">
-        <van-button
-          type="primary"
-          color="#3f72af"
-          block
-          @click="handleClickCreditPage"
-          >{{ t("Margin financing") }}</van-button
-        >
+        <van-button type="primary" :color="item.status == 1 ? '#007aff' : '#ccc'" block
+          @click="handleClickCreditPage">{{ t("Margin financing") }}</van-button>
       </div>
       <div class="flex-1">
-        <van-button
-          type="primary"
-          block
-          @click="handleClickSubmit"
-          :color="item.status == 1 ? '#007aff' : '#ccc'"
-        >
-          {{ t("subscription") }}</van-button
-        >
+        <van-button type="primary" block @click="handleClickSubmit" :color="item.status == 1 ? '#007aff' : '#ccc'">
+          {{ t("subscription") }}</van-button>
       </div>
     </div>
   </div>
-  <div
-    class="order-item-content w-full px-12 py-24 font-size-12 flex flex-col gap-10px"
-    v-if="props.itemType == 'order'"
-  >
+  <div class="order-item-content w-full px-12 py-24 font-size-12 flex flex-col gap-10px"
+    v-if="props.itemType == 'order'">
     <ipoItemTop :item="_item" :item-type="'order'"></ipoItemTop>
     <ipoOrderItemInfo :item="_item"></ipoOrderItemInfo>
     <div class="flex gap-12">
       <div class="flex-1"></div>
       <div class="flex-1">
-        <van-button
-          type="primary"
-          block
-          @click="handleClickSubmit"
-          :color="item.status == 1 ? '#007aff' : '#ccc'"
-          >{{ orderBtnText }}</van-button
-        >
+        <van-button type="primary" block @click="handleClickSubmit" :color="item.status == 1 ? '#007aff' : '#ccc'">{{
+          orderBtnText }}</van-button>
       </div>
     </div>
   </div>
@@ -118,6 +92,7 @@ const _index = computed(() => {
   return 1;
 });
 const handleClickCreditPage = () => {
+  if (_item.value.status != 1) return
   router.push(
     `/ipo/creditPage?assetId=${_item.value.assetId}&ipoId=${_item.value.ipoId}`
   );
