fd395c0f50ce04d96e202461fe759c685e6fc7cd
cykingkong <847939497@qq.com>
2025-07-09 04:50:30 +0800

    update

--- src/pages/conversion/index.vue
+++ src/pages/conversion/index.vue
@@ -3,19 +3,14 @@
     <div class="title font-size-20 mb-12" @click="showPicker = true">
       {{ t("profile.conversion") }}
     </div>
-    <div
-      class="transfer w-full rounded-10 p-12 font-size-14 flex items-center mb-12"
-    >
+    <div class="transfer w-full rounded-10 p-12 font-size-14 flex items-center mb-12">
       <div class="left w-90% flex-shrink-0">
         <div class="li flex flex-shrink-0 gap-30">
           <div class="l flex items-center gap-8">
             <van-icon name="gold-coin-o" class="block" />
             {{ t("from") }}
           </div>
-          <div
-            class="mid flex-1 flex justify-between items-center"
-            @click="handleClickPicker(0)"
-          >
+          <div class="mid flex-1 flex justify-between items-center" @click="handleClickPicker(0)">
             {{ form.baseAssetIdDesc }}
             <van-icon name="arrow-down" class="w-20 h-20" />
           </div>
@@ -26,10 +21,7 @@
             <van-icon name="gold-coin-o" class="block" />
             {{ t("to") }}
           </div>
-          <div
-            class="mid flex-1 flex justify-between items-center"
-            @click="handleClickPicker(1)"
-          >
+          <div class="mid flex-1 flex justify-between items-center" @click="handleClickPicker(1)">
             {{ form.quoteAssetIdDesc }}
             <van-icon name="arrow-down" class="w-20 h-20" />
           </div>
@@ -39,13 +31,8 @@
         <van-icon name="exchange" size="20px" class="rotate-90" />
       </div>
     </div>
-    <inputCom
-      :label="t('price')"
-      class="mb-12"
-      :placeholder="t('input.PleaseEnter')"
-      v-model:value="form.amount"
-      :tips="tips"
-    >
+    <inputCom :label="t('price')" class="mb-12" :placeholder="t('input.PleaseEnter')" v-model:value="form.amount"
+      :tips="tips">
       <template #sendCode>
         <div class="absolute right-0 font-size-12 sendCode">
           {{ form.baseAssetIdDesc }}
@@ -59,12 +46,8 @@
     }}</van-button>
 
     <van-popup v-model:show="showPicker" destroy-on-close position="bottom">
-      <van-picker
-        :columns="columns"
-        :model-value="popType ? [form.baseAssetId] : [form.quoteAssetId]"
-        @confirm="onConfirm"
-        @cancel="showPicker = false"
-      />
+      <van-picker :columns="columns" :model-value="popType ? [form.baseAssetId] : [form.quoteAssetId]"
+        @confirm="onConfirm" @cancel="showPicker = false" />
     </van-popup>
   </div>
 </template>
@@ -81,6 +64,7 @@ const { t } = useI18n();
 
 const columns = ref([]);
 const showPicker = ref(false);
+const router = useRouter();
 const form = reactive({
   quoteAssetId: 0,
   quoteAssetIdDesc: "",
@@ -120,13 +104,11 @@ watch(
         conversionInfoData.value.baseAssetSymbolName,
         addCommasToNumber(Number(price.value))
       );
-      tips.value = `${t("Available balance")}:${
-        conversionInfoData.value.baseAssetSymbolName
-      } ${addCommasToNumber(Number(price.value))}`;
+      tips.value = `${t("Available balance")}:${conversionInfoData.value.baseAssetSymbolName
+        } ${addCommasToNumber(Number(price.value))}`;
     } else {
-      tips.value = `${t("Available balance")}:${
-        conversionInfoData.value.quoteAssetSymbolName
-      } ${addCommasToNumber(Number(quotesPrice.value))} `;
+      tips.value = `${t("Available balance")}:${conversionInfoData.value.quoteAssetSymbolName
+        } ${addCommasToNumber(Number(quotesPrice.value))} `;
     }
   }
 );
@@ -138,16 +120,14 @@ watch(
         ? Number(form.amount) * conversionInfoData.value.baseAssetAmountRate
         : 0;
       console.log(canhasAmount.value, "canhasAmount.value");
-      canhasAmountTips.value = `${t("Estimated to obtain")}:${
-        conversionInfoData.value.quoteAssetSymbolName
-      } ${addCommasToNumber(canhasAmount.value)}`;
+      canhasAmountTips.value = `${t("Estimated to obtain")}:${conversionInfoData.value.quoteAssetSymbolName
+        } ${addCommasToNumber(canhasAmount.value)}`;
     } else {
       canhasAmount.value = form.amount
         ? Number(form.amount) * conversionInfoData.value.quoteAssetAmountRate
         : 0;
-      canhasAmountTips.value = `${t("Estimated to obtain")}:${
-        conversionInfoData.value.baseAssetSymbolName
-      } ${addCommasToNumber(canhasAmount.value)}`;
+      canhasAmountTips.value = `${t("Estimated to obtain")}:${conversionInfoData.value.baseAssetSymbolName
+        } ${addCommasToNumber(canhasAmount.value)}`;
     }
   }
 );
@@ -179,11 +159,11 @@ const getBalanceColumns = async () => {
   if (code == 200) {
     columns.value = data.rows
       ? data.rows.map((item) => {
-          return {
-            text: item.assetInfo.symbol,
-            value: item.assetId,
-          };
-        })
+        return {
+          text: item.assetInfo.symbol,
+          value: item.assetId,
+        };
+      })
       : [];
     assetsListIds.value = {
       quoteAssetId: data.rows[0].assetId,
@@ -199,9 +179,9 @@ const getBalanceColumns = async () => {
 };
 const maxAmount = () => {
   if (balanceType.value == 0) {
-    form.amount = conversionInfoData.value.baseAssetBalance;
+    form.amount = conversionInfoData.value.baseAssetBalance + "";
   } else {
-    form.amount = conversionInfoData.value.quoteAssetBalance;
+    form.amount = conversionInfoData.value.quoteAssetBalance + "";
   }
 };
 
@@ -215,13 +195,11 @@ const getInfo = async () => {
     price.value = `${data.baseAssetBalance}`;
     quotesPrice.value = `${data.quoteAssetBalance}`;
     if (balanceType.value == 0) {
-      tips.value = `${t("Available balance")}${
-        conversionInfoData.value.baseAssetSymbolName
-      } ${addCommasToNumber(Number(price.value))} `;
+      tips.value = `${t("Available balance")}${conversionInfoData.value.baseAssetSymbolName
+        } ${addCommasToNumber(Number(price.value))} `;
     } else {
-      tips.value = `${t("Available balance")}${
-        conversionInfoData.value.quoteAssetSymbolName
-      } ${addCommasToNumber(Number(quotesPrice.value))} `;
+      tips.value = `${t("Available balance")}${conversionInfoData.value.quoteAssetSymbolName
+        } ${addCommasToNumber(Number(quotesPrice.value))} `;
     }
   }
 };
@@ -239,13 +217,12 @@ const handleClickSubmit = async () => {
     amount: Number(form.amount.replace(/,/g, "")),
   });
   if (code == 200) {
-    console.log(data);
-
     showSuccessToast({
       zIndex: 9999,
+      onClose: () => {
+        router.push("/deal");
+      },
     });
-    form.amount = "";
-    getInfo();
   }
 };
 onMounted(async () => {
