469db2db5740494db429e8e34dbf59d033b2689f
cykingkong <847939497@qq.com>
2025-07-17 22:56:34 +0800

    fix

--- src/pages/quotes/component/accountChangeCom/tab-item.vue
+++ src/pages/quotes/component/accountChangeCom/tab-item.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="">
         <div class="account-change-item flex py-12 px17">
-            <div class="left flex flex-items-center gap-8 flex-shrink-0">
+            <div class="left flex flex-items-center gap-8 flex-shrink-0 w-60%">
                 <img src="@/assets/image/assets-center/in.png" alt="" class="up w-20 h-20" v-show="item.type == 1">
                 <img src="@/assets/image/assets-center/out.png" alt="" class="up w-20 h-20" v-show="item.type == 2">
                 <div class="left-info">
@@ -10,8 +10,9 @@
                 </div>
             </div>
             <div class="right flex-1 text-align-right">
-                <div class="font-size-20" :class="item.type == 1 ? 'up' : 'down'">{{ item.type == 1 ? '+' : '-' }}{{
-                    addCommasToNumber(item.amount) }}</div>
+                <div class="font-size-20" :class="item.type == 1 ? 'up' : 'down'">{{ item.type == 1 ? '+' : '-' }} {{
+                    item.assetInfo ? item.assetInfo.unit : '' }} {{
+                        addCommasToNumber(item.amount) }}</div>
                 <div class="time font-size-12 text-gray mt-10">{{ dayjs(item.createdAt).format("DD/MM/YYYY HH:mm:ss") }}
                 </div>
             </div>
